767d2d9e1b67a0e7128c78a990a6bb7c2a198c06
Pragati Sanepara <pragati@atharvasystem.com>
2022-12-07 15:23:14 +0530

    header created

---
+++ src/Component/header.tsx
@@ -0,0 +1,105 @@
+import logo from "./../assets/images/atharva-brand-logo-dark.png";
+import { useState } from "react";
+
+function Header(props: any) {
+  const [navbarOpen, setNavbarOpen] = useState(false);
+
+  return (
+    <>
+      <nav
+        className={
+          (props.transparent
+            ? "top-0 absolute z-50 w-full"
+            : "relative shadow-lg bg-white shadow-lg") +
+          " flex flex-wrap items-center justify-between px-2 py-3 "
+        }
+      >
+        <div className="container px-4 mx-auto flex flex-wrap items-center justify-between">
+          <div className="w-full relative flex justify-between lg:w-auto lg:static lg:block lg:justify-start">
+            <a
+              href="/"
+              className={
+                (props.transparent ? "text-white" : "text-gray-800") +
+                " text-sm font-bold leading-relaxed inline-block mr-4 py-2 whitespace-nowrap uppercase"
+              }
+            >
+              <img src={logo} alt="Atharva System" />
+            </a>
+            <button
+              className="cursor-pointer text-xl leading-none px-3 py-1 border border-solid border-transparent rounded bg-transparent block outline-none focus:outline-none"
+              type="button"
+              onClick={() => setNavbarOpen(!navbarOpen)}
+            >
+              <i
+                className={
+                  (props.transparent ? "text-white" : "text-gray-800") +
+                  " fas fa-bars"
+                }
+              ></i>
+            </button>
+          </div>
+          <div
+            className={
+              "lg:flex flex-grow items-center bg-white lg:bg-transparent lg:shadow-none" +
+              (navbarOpen ? " block rounded shadow-lg" : "")
+            }
+            id="example-navbar-warning"
+          >
+            <ul className="flex flex-col lg:flex-row list-none mr-auto">
+              <li className="flex items-center">
+                <a
+                  className={
+                    (props.transparent
+                      ? "lg:text-white lg:hover:text-gray-300 text-gray-800"
+                      : "text-gray-800 hover:text-gray-600") +
+                    " px-3 py-4 lg:py-2 flex items-center text-xs uppercase font-bold"
+                  }
+                  href="/candidate-table"
+                >
+                  Candidate
+                </a>
+              </li>
+              <li>
+                <a
+                  className={
+                    (props.transparent
+                      ? "lg:text-white lg:hover:text-gray-300 text-gray-800"
+                      : "text-gray-800 hover:text-gray-600") +
+                    " px-3 py-4 lg:py-2 flex items-center text-xs uppercase font-bold"
+                  }
+                  href="/candidateall-table"
+                >
+                  <i
+                    className={
+                      (props.transparent
+                        ? "lg:text-gray-300 text-gray-500"
+                        : "text-gray-500") + "text-lg leading-lg mr-2"
+                    }
+                  />
+                  Question
+                </a>
+              </li>
+            </ul>
+            <ul className="flex flex-col lg:flex-row list-none lg:ml-auto">
+              <li className="flex items-center">
+                <button
+                  className={
+                    (props.transparent
+                      ? "bg-white text-gray-800 active:bg-gray-100"
+                      : "bg-pink-500 text-white active:bg-pink-600") +
+                    " text-xs font-bold uppercase px-4 py-2 rounded shadow hover:shadow-md outline-none focus:outline-none lg:mr-1 lg:mb-0 ml-3 mb-3"
+                  }
+                  type="button"
+                  style={{ transition: "all .15s ease" }}
+                >
+                  <a href="/logout">LogOut</a>
+                </button>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </>
+  );
+}
+export default Header;
