8fffa1ef35304675cda29ac9b692493332c01923
Pragati Sanepara <pragati@atharvasystem.com>
2022-12-08 17:04:21 +0530

    update question details page

--- src/Component/QuestionDetails.tsx
+++ src/Component/QuestionDetails.tsx
@@ -1,4 +1,4 @@
-import React, { useRef } from "react";
+import React, { useCallback, useContext, useRef } from "react";
 import { useEffect, useState } from "react";
 import axios from "axios";
 import { useParams } from "react-router-dom";
@@ -9,19 +9,21 @@ import InputField from "./Input";
 interface IQuestiondata {
   question?: string;
   optionType?: String;
-  ans?: string;
+  ans?: string[];
   _id?: string;
+  options?: any[];
 }
 
 const QuestionDetail = () => {
   const [isLoad, setIsLoad] = useState(false);
   const { id } = useParams();
+  // const questionContextValue = useContext(QuestionContext);
 
   const [data, setData] = useState<IQuestiondata>({});
   const [question, setQuestion] = useState<string>("");
   const [queryAns, setQueryAns] = useState<string>("");
 
-  useEffect(() => {
+  const fetchQuestion = useCallback(() => {
     axios
       .get(`${process.env.REACT_APP_API}/management/question/get/${id}`)
       .then((res: any) => {
@@ -34,9 +36,53 @@ const QuestionDetail = () => {
   }, []);
 
   useEffect(() => {
+    fetchQuestion();
+  }, [fetchQuestion]);
+
+  useEffect(() => {
     setQuestion(data.question ? data.question : "");
-    setQueryAns(data.ans ? data.ans : "");
+    setQueryAns(data.ans?.length ? data.ans[0] : "");
   }, [data]);
+
+  // const handleOnChangeCheckbox = (selectedIndex : number, e : any) => {
+  //   let options = data.options;
+  //   if(options)
+  //   options[selectedIndex].value = e.target.checked;
+
+  //   let question = {
+  //     ...data,
+  //     options: options,
+  //   };
+  //   props.updateQuestion(question);
+  // };
+
+  // const handleOnChangeRadio = (selectedIndex, e) => {
+  //   let options = props.question.options;
+  //   options.map(
+  //     (data) => (data.value = data.title === e.target.value ? true : false)
+  //   );
+
+  //   let question = {
+  //     ...props.question,
+  //     options: options,
+  //   };
+  //   props.updateQuestion(question);
+  // };
+
+  // const handleOnChangeQuery = (index, e, id) => {
+  //   console.log(e);
+  //   let options = props.question.options;
+  //   options.map((data) => {
+  //     data.query = e.target.value;
+  //     return (data.value = data.query ? true : false);
+  //   });
+
+  //   let question = {
+  //     ...props.question,
+  //     options: options,
+  //   };
+  //   props.updateQuestion(question);
+  // };
 
   return (
     <>
@@ -51,19 +97,59 @@ const QuestionDetail = () => {
             onChange={setQuestion}
           />
 
+          <ul className="question-list">
+            {data?.options?.map((option, index) => {
+              return (
+                <li key={`${index}-${data?._id}`}>
+                  {data?.optionType === "Multiple" ? (
+                    <input
+                      type="checkbox"
+                      id={`custom-checkbox-${index}-${data?._id}`}
+                      name={option?.title}
+                      checked={data.ans?.includes(option?.title)}
+                      // onChange={(e) => handleOnChangeCheckbox(index, e)}
+                    />
+                  ) : data?.optionType === "Query" ? (
+                    <div>
+                       <label htmlFor="query">SQL Query Answer</label>
+                      <textarea
+                        id={`custom-checkbox-${index}-${data?._id}`}
+                        name={option?._id}
+                        defaultValue={option?.query}
+                        // onChange={(e) => handleOnChangeQuery(index, e, option._id)}
+                      ></textarea>
+                    </div>
+                  ) : (
+                    <input
+                      type="radio"
+                      value={option?.title}
+                      id={`custom-checkbox-${index}-${data?._id}`}
+                      name={option?.title}
+                      checked={data.ans?.includes(option?.title)}
+                      // onChange={(e) => handleOnChangeRadio(index, e)}
+                    />
+                  )}
+
+                  <label htmlFor={`custom-checkbox-${index}-${data?._id}`}>
+                    {option?.title}
+                  </label>
+                </li>
+              );
+            })}
+          </ul>
+
           <div>
-            
             {data?.optionType === "Query" ? (
               <div>
-              <label htmlFor="query">SQL Query Answer</label>
-              <textarea
-                id="query"
-                placeholder="Type Query"
-                className="form-control"
-                value={queryAns}
-                onChange={(e) => setQueryAns(e.target.value)}
-                required
-              ></textarea>
+                <label htmlFor="query">SQL Query Answer</label>
+                <textarea
+                  id="query"
+                  placeholder="Type Query"
+                  className="form-control"
+                  value={queryAns}
+                  onChange={(e) => setQueryAns(e.target.value)}
+                  required
+                ></textarea>
               </div>
             ) : (
               <InputField
